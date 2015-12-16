ce1974c7fbcf8208440f756accd8fa0d2290583c
Milton <tropa9527@gmail.com>
2015-12-16 00:29:24 +0000

    Added fields for the form to handle kennel booking

--- src/main/java/server/IndexServlet.java
+++ src/main/java/server/IndexServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
 
 import java.io.IOException;
@@ -10,6 +5,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uk.ac.ncl.csc3422.kennelbooking.Kennel;
+import uk.ac.ncl.csc3422.kennelbooking.KennelFactory;
+import uk.ac.ncl.csc3422.kennelbooking.KennelReport;
 
 /**
  *
@@ -17,20 +15,35 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class IndexServlet extends HttpServlet {
 
+	private Kennel kennel = KennelFactory.initialiseKennel();
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("<html><body><h3>doGet method!!!!!</h3></body></html>");
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//String uname = request.getParameter("username");
-		response.getWriter().print("<html><body>IndexServlet.doPost works!</body></html>");
+		request.setAttribute("username", request.getParameter("username"));
+		
+		String[] reportLines = getLines(KennelReport.generateReport(kennel));
+		for (int i = 0; i < reportLines.length; i++) {
+			request.setAttribute("report_line" + (i + 1), reportLines[i]);
+		}
+		
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 	
-//	private String includeHTMLbody() {
-//		
-//	}
+	private String[] getLines(String report) {
+		String[] lines = new String[8];
+		lines[0] = report.substring(0, 92);
+		lines[1] = report.substring(93, 140);
+		lines[2] = report.substring(141, 188);
+		lines[3] = report.substring(189, 236);
+		lines[4] = report.substring(237, 285);
+		lines[5] = report.substring(286, 334);
+		lines[6] = report.substring(335, 383);
+		lines[7] = report.substring(384, 431);
+		return lines;
+	}
 }
--- target/classes/webapp/index.jsp
+++ target/classes/webapp/index.jsp
@@ -1,18 +1,26 @@
 <html>
-	<head><title>First JSP</title></head>
+	<head><title>Kennel Booking</title></head>
 	<body>
-		<%
-		  double num = Math.random();
-		  if (num > 0.95) {
-		%>
-		<h2>You'll have a luck day!</h2><p>(<%= num %>)</p>
-		<%
-		  } else {
-		%>
-		<h2>Well, life goes on ... </h2><p>(<%= num %>)</p>
-		<%
-		  }
-		%>
-		<a href="<%= request.getRequestURI() %>"><h3>Try Again</h3></a>
+		<p>Hello ${username}</p>
+		<div>
+			<b>Vacancies in the kennel</b><br>
+			${report_line1}<br>
+			${report_line2}<br>
+			${report_line3}<br>
+			${report_line4}<br>
+			${report_line5}<br>
+			${report_line6}<br>
+			${report_line7}<br>
+			${report_line8}<br>
+		</div>
+		<p><b>Dog size</b></p>
+		<select name="dogsize">
+			<option value="small">Small</option>
+			<option value="medium">Medium</option>
+			<option value="giant">Giant</option>
+		</select>
+		<p><b>Name of your dog</b></p>
+		<input type="text" name="dogname">
+		<p><input type="submit" name="book" value="Book"></p>
 	</body>
 </html>
